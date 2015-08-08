f2df2144c2d2a351dd44551e919ac22f3f7736df
alberto <infjaf@gmail.com>
2015-08-08 18:48:15 +0200

    test on writting files

---
+++ src/test/java/com/linuxense/javadbf/NullOutputStream.java
@@ -0,0 +1,54 @@
+package com.linuxense.javadbf;
+
+import java.io.IOException;
+import java.io.OutputStream;
+
+
+public class NullOutputStream extends OutputStream {
+    
+	private long count = 0;
+	
+
+    /**
+     * Constructor
+     */
+    public NullOutputStream() {
+        super();
+    }
+    /**
+     * Does nothing - output to <code>/dev/null</code>.
+     * @param b The bytes to write
+     * @param off The start offset
+     * @param len The number of bytes to write
+     */
+    @Override
+    public void write(byte[] b, int off, int len) {
+        this.count+= len;
+    }
+
+    /**
+     * Does nothing - output to <code>/dev/null</code>.
+     * @param b The byte to write
+     */
+    @Override
+    public void write(int b) {
+        this.count++;
+    }
+
+    /**
+     * Does nothing - output to <code>/dev/null</code>.
+     * @param b The bytes to write
+     * @throws IOException never
+     */
+    @Override
+    public void write(byte[] b) throws IOException {
+        if (b != null) {
+        	this.count += b.length;
+        }
+    }
+    
+    public long getCount() {
+    	return this.count;
+    }
+
+}
--- src/test/java/com/linuxense/javadbf/SimpleTest.java
+++ src/test/java/com/linuxense/javadbf/SimpleTest.java
@@ -1,5 +1,8 @@
 package com.linuxense.javadbf;
 
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
@@ -10,45 +13,135 @@ import org.junit.Test;
 public class SimpleTest {
 
 	@Test
-	public void testContinents() throws DBFException, IOException {
-		testSize("continents", 1, 7);
+	public void testReadContinents() throws DBFException, IOException {
+		testReadDBFFile("continents", 1, 7);
 	}
 	
 	@Test
-	public void testBooks() throws DBFException, IOException {
-		testSize("books", 11, 10);
+	public void testReadBooks() throws DBFException, IOException {
+		testReadDBFFile("books", 11, 10);
 	}
 	
 	@Test
-	public void testCountries() throws DBFException, IOException {
-		testSize("countries", 29, 177);
+	public void testReadCountries() throws DBFException, IOException {
+		testReadDBFFile("countries", 29, 177);
 	}
+	/**
+	 * Open a file generated with javadbf
+	 * @throws DBFException
+	 * @throws IOException
+	 */
+	@Test
+	public void testReadJavaDbf() throws DBFException, IOException {
+		testReadDBFFile("javadbf", 3, 3);
+	}
+	
+	@Test
+	public void testWriteAndReadAgain() throws DBFException, IOException {
+		 // let us create field definitions first
+		// we will go for 3 fields
+		//
+		DBFField fields[] = new DBFField[3];
 
-	private void testSize(String fileName, int columns, int rows) throws DBFException, IOException {
-		InputStream inputStream = null;
-		
+		fields[0] = new DBFField();
+		fields[0].setName("emp_code");
+		fields[0].setDataType(DBFField.FIELD_TYPE_C);
+		fields[0].setFieldLength(10);
+
+		fields[1] = new DBFField();
+		fields[1].setName("emp_name");
+		fields[1].setDataType(DBFField.FIELD_TYPE_C);
+		fields[1].setFieldLength(20);
+
+		fields[2] = new DBFField();
+		fields[2].setName("salary");
+		fields[2].setDataType(DBFField.FIELD_TYPE_N);
+		fields[2].setFieldLength(12);
+		fields[2].setDecimalCount(2);
+
+		DBFWriter writer = new DBFWriter();
+		writer.setFields(fields);
+
+		// now populate DBFWriter
+		//
+
+		Object rowData[] = new Object[3];
+		rowData[0] = "1000";
+		rowData[1] = "John";
+		rowData[2] = new Double(5000.00);
+
+		writer.addRecord(rowData);
+
+		rowData = new Object[3];
+		rowData[0] = "1001";
+		rowData[1] = "Lalit";
+		rowData[2] = new Double(3400.00);
+
+		writer.addRecord(rowData);
+
+		rowData = new Object[3];
+		rowData[0] = "1002";
+		rowData[1] = "Rohit";
+		rowData[2] = new Double(7350.00);
+
+		writer.addRecord(rowData);
+
+		ByteArrayOutputStream out = null;
 		try {
-			inputStream  = new FileInputStream("src/test/resources/" + fileName + ".dbf");
-		      DBFReader reader = new DBFReader( inputStream); 
-		      
-		      int numberOfFields = reader.getFieldCount();
-		      Assert.assertEquals(columns, numberOfFields);
-		      for( int i=0; i<numberOfFields; i++) {
-			        DBFField field = reader.getField(i);
-			        Assert.assertNotNull(field.getName());
-			   }
-		      Object[] rowObject;
-		      int countedRows = 0;
-		      while( (rowObject = reader.nextRecord()) != null) {
-		    	  Assert.assertEquals(numberOfFields, rowObject.length);
-		    	  countedRows++;
-		      }
-		      Assert.assertEquals(rows, countedRows);
-		      
+			out = new ByteArrayOutputStream();
+			writer.write(out);
+		} finally {
+			if (out != null) {
+				out.close();
+			}
+		}
+		
+		byte[] data = out.toByteArray();
+		Assert.assertEquals(259, data.length);
+		
+		ByteArrayInputStream bis = null;
+		try {
+			bis = new ByteArrayInputStream(data);
+			testReadDBFFile(bis, 3, 3);
 		}
 		finally {
-			inputStream.close();
-		}		
+			if (bis != null) {
+				bis.close();
+			}
+		}
 		
+	}
+
+	private void testReadDBFFile(String fileName, int columns, int rows) throws DBFException, IOException {
+		testReadDBFFile(new File("src/test/resources/" + fileName + ".dbf"), columns, rows);
+	}
+
+	private void testReadDBFFile(File file, int columns, int rows) throws DBFException, IOException {
+		InputStream inputStream = null;
+		try {
+			inputStream = new FileInputStream(file);
+			testReadDBFFile(inputStream, columns, rows);
+		} finally {
+			inputStream.close();
+		}
+
+	}
+
+	private void testReadDBFFile(InputStream inputStream, int columns, int rows) throws DBFException {
+		DBFReader reader = new DBFReader(inputStream);
+
+		int numberOfFields = reader.getFieldCount();
+		Assert.assertEquals(columns, numberOfFields);
+		for (int i = 0; i < numberOfFields; i++) {
+			DBFField field = reader.getField(i);
+			Assert.assertNotNull(field.getName());
+		}
+		Object[] rowObject;
+		int countedRows = 0;
+		while ((rowObject = reader.nextRecord()) != null) {
+			Assert.assertEquals(numberOfFields, rowObject.length);
+			countedRows++;
+		}
+		Assert.assertEquals(rows, countedRows);
 	}
 }
